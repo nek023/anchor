983685caa1cba048473ccd335a83a4442f43706c
questbeat <questbeat@gmail.com>
2016-08-11 16:16:55 +0900

    [#3] Remove redux-logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,18 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
-import createLogger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
 import rootSaga from "./sagas";
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
-  const logger = createLogger();
   const store = createStore(
     rootReducer,
     initialState,
     applyMiddleware(
-      sagaMiddleware,
-      logger
+      sagaMiddleware
     )
   );
 
