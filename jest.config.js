d0e61dc446fca13ee1e3a1b9249e95faf2650d8f
questbeat <questbeat@gmail.com>
2020-01-26 03:13:21 +0900

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
+/* eslint-disable no-undef */
+
 module.exports = {
   preset: 'ts-jest',
-  roots: [
-    '<rootDir>/src'
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/src/setupTests.ts'
-  ],
-  snapshotSerializers: [
-    'enzyme-to-json/serializer'
-  ]
-};
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+}
