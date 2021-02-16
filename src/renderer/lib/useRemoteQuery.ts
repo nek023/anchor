ab795e7dd8733f7f61b0b6e823988e8146779616
questbeat <questbeat@gmail.com>
2021-02-17 00:28:19 +0900

    Refine useRemoteQuery

--- src/renderer/lib/useRemoteQuery.ts
+++ src/renderer/lib/useRemoteQuery.ts
@@ -1,11 +1,27 @@
-import { useRemoteMessage } from "./useRemoteMessage";
-import { MessageType } from "../../common/ipc";
+import { useCallback, useEffect } from "react";
+import { Message, MessageType } from "../../common/ipc";
+
+type ExtensionMessageEventCallback = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  message: Message,
+  sender: chrome.runtime.MessageSender,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sendResponse: (response?: any) => void
+) => void;
 
 export const useRemoteQuery = (callback: (query: string) => void) => {
-  useRemoteMessage((message, sender, sendResponse) => {
-    if (message.type === MessageType.SetQuery) {
-      callback(message.payload.query);
-      sendResponse(true);
-    }
-  });
+  const handleMessage = useCallback<ExtensionMessageEventCallback>(
+    (message, sender, sendResponse) => {
+      if (message.type === MessageType.SetQuery) {
+        callback(message.payload.query);
+        sendResponse(true);
+      }
+    },
+    [callback]
+  );
+
+  useEffect(() => {
+    chrome.runtime.onMessage.addListener(handleMessage);
+    return () => chrome.runtime.onMessage.removeListener(handleMessage);
+  }, [handleMessage]);
 };
